4fb14fd2e7d46b9adb56461c38ba9c2cadaa8aa4
LAPTOP-3QO177P3\shahp <shahprenik@gmail.com>
2024-12-09 22:32:16 +0530

    Update player settings

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,8 +1,8 @@
-const cacheName = "DefaultCompany-OrganizeItems-1.0";
+const cacheName = "psgaming09-OrganizeItems-1.0";
 const contentToCache = [
     "Build/87b20b7854cef650e3e09f29d48f6029.loader.js",
     "Build/cc48a754ca17ba7b80f9c4fe3c5a1544.framework.js",
-    "Build/b00d3e2426da75b6d0e066b23ecf4cc0.data",
+    "Build/1ddf3a2cf5484ee367a8d0471979ff69.data",
     "Build/09bb600d3aca31eeda63e9c4734225b6.wasm",
     "TemplateData/style.css"
 
