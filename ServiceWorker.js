1cd8b9e518417b42a2a3d4e5ca55466c1ff29307
LAPTOP-3QO177P3\shahp <shahprenik@gmail.com>
2024-12-14 10:54:35 +0530

    Update background 3d model texture

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "psgaming09-OrganizeItems-1.0";
 const contentToCache = [
     "Build/87b20b7854cef650e3e09f29d48f6029.loader.js",
     "Build/cc48a754ca17ba7b80f9c4fe3c5a1544.framework.js",
-    "Build/1ddf3a2cf5484ee367a8d0471979ff69.data",
+    "Build/febf878a81b98db7ed4c68d919bb1fc6.data",
     "Build/09bb600d3aca31eeda63e9c4734225b6.wasm",
     "TemplateData/style.css"
 
