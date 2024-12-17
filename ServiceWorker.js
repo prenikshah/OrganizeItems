37aab88654eb6afef03bf4ac11181496acc6ea65
LAPTOP-3QO177P3\shahp <shahprenik@gmail.com>
2024-12-18 02:53:59 +0530

    update event handler code

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "psgaming09-OrganizeItems-1.0";
 const contentToCache = [
-    "Build/2b2f20f9e6e38dc1f126847bc44667e3.loader.js",
+    "Build/86a6b753ef93034def206d78a9dc53fb.loader.js",
     "Build/38b0dbafca586f9236fce42d04ef6983.framework.js",
-    "Build/d7e9edb10281934af499b1f85d09b2b8.data",
-    "Build/8649c26c57a0327755fbb221c846898f.wasm",
+    "Build/3bcc25a1cf31df52e7e893aab4d16919.data",
+    "Build/6df089459da5a9e9a3d4e0cac1e011f0.wasm",
     "TemplateData/style.css"
 
 ];
